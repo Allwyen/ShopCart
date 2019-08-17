bc7ba1cca558380d53201fcc3a71071e15cd1f08
Allwyen <allwyen27@gmail.com>
2019-08-17 15:46:05 +0530

    ShopCart

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.set('view engine','ejs');
 app.use(Express.static(__dirname+'/public'));
 
 app.get('/',(req,res)=>{
-    res.render('index');
+    res.render('index',{title:'ShopCart'});
+});
+
+app.get('/items',(req,res)=>{
+    res.render('items',{title:'ShopCart'});
 });
 
 app.listen(process.env.PORT || 3456,()=>{
